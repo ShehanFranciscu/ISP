e7f0ad95b7c1f47f3e4abff108a21efc7d9f6bfa
ShehanFranciscu <83289805+ShehanFranciscu@users.noreply.github.com>
2022-10-23 14:26:00 +0530

    Add the merged file(Not finalized)

---
+++ Merged.py
@@ -0,0 +1,15 @@
+
+print("Program Name 1 ")
+print("Program Name 2 ")
+print("Program Name 3 ")
+
+programID = int(input("Enter Program ID : "))
+
+if (programID == 1):
+  print("Program 1")
+elif (programID == 2):
+  print("Program 2")
+elif (programID == 3):
+  print("Program 3")
+else:
+  print("Wrong ID !!!")
